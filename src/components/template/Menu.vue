eebb9bff490bffec001dfe06cec48e681b702e8b
Henrique <ricogx@hotmail.com>
2019-05-31 01:38:55 -0300

    componente tree removido e nav criado

--- src/components/template/Menu.vue
+++ src/components/template/Menu.vue
@@ -3,73 +3,100 @@
         <div class="menu-filter">
             <i class="fa fa-search fa-lg"></i>
             <input type="text" placeholder="Digite para filtrar" 
-            v-model="treeFilter" class="filter-field">
+            v-model="filtroMenu" class="filter-field">
         </div>
-        <Tree :data="items" :options="options" :filter="treeFilter" ref="tree" />
+        <span class="tree-filter-empty" v-if="items.length < 1">Categoria não encontrada</span>
+        <nav >
+            <ul>
+                <router-link active-class="active" exact v-if="item.visible" :to="item.path" tag="li" v-for="item in items" :key="item.id" >
+                    <span>{{item.text}}</span>
+                </router-link>
+            </ul>
+        </nav>
     </aside>
     
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-import Tree from 'liquor-tree'
 import {userKey} from '@/global'
 export default {
     name: 'Menu',
-    components: {Tree},
     computed: mapGetters(['isMenuVisible']),
     data(){
         return {
-            treeFilter: '',
-            e: false,
+            filtroMenu: '',
             items: [
-            {text: 'Home'},
-            {text: 'Usuarios'},
-            {text: 'Livros'},
-            {text: 'Editoras'},
-            {text: 'Autores'},
-            {text: 'Assuntos'},
-            {text: 'Categorias'}
+            {text: 'Home', path: '/dashboard/home', visible: true},
+            {text: 'Usuarios', path: '/dashboard/usuarios', visible: this.hasRole('BUSCAR_USUARIO')},
+            {text: 'Livros', path: '/dashboard/livros', visible: this.hasRole('BUSCAR_LIVRO')},
+            {text: 'Editoras', path: '/dashboard/editoras', visible: this.hasRole('BUSCAR_EDITORA')},
+            {text: 'Autores', path: '/dashboard/autores', visible: this.hasRole('BUSCAR_AUTOR')},
+            {text: 'Assuntos', path: '/dashboard/assuntos', visible: this.hasRole('BUSCAR_ASSUNTO')},
+            {text: 'Categorias', path: '/dashboard/categorias', visible: this.hasRole('BUSCAR_DESCRICAO')}
           ],
-          options: {
-            filter: {
-                emptyText: 'Categoria não encontrada'
-            }
-          }
+        }
+    },
+    watch: {
+        filtroMenu(newValue, oldValue){
+            let items = [
+            {text: 'Home', path: '/dashboard/home', visible: true},
+            {text: 'Usuarios', path: '/dashboard/usuarios', visible: this.hasRole('BUSCAR_USUARIO')},
+            {text: 'Livros', path: '/dashboard/livros', visible: this.hasRole('BUSCAR_LIVRO')},
+            {text: 'Editoras', path: '/dashboard/editoras', visible: this.hasRole('BUSCAR_EDITORA')},
+            {text: 'Autores', path: '/dashboard/autores', visible: this.hasRole('BUSCAR_AUTOR')},
+            {text: 'Assuntos', path: '/dashboard/assuntos', visible: this.hasRole('BUSCAR_ASSUNTO')},
+            {text: 'Categorias', path: '/dashboard/categorias', visible: this.hasRole('BUSCAR_DESCRICAO')}
+          ];
+            this.items =items.filter(x=> x.visible === true).filter(x => x.text.toLowerCase().includes(newValue.toLowerCase()));
         }
     },
     methods: {
-        onNodeSelect(node){
-            this.$router.push(`/dashboard/${node.text.toLowerCase()}`)
-            
+        toRoute(path){
+            this.$router.push(path);
             if(this.$mq === 'xs' || this.$mq === 'sm'){
                 this.$store.dispatch('toggleMenu', false)
             }
+        },
+        hasRole(role){
+            let roles = JSON.parse(localStorage.getItem(userKey)).user.permissoes;
+            if(roles.includes(role)){
+                return true;
+            }
+            return false;
         }
-    },
-    mounted(){
-        // let roles = JSON.parse(localStorage.getItem(userKey)).user.permissoes;
-        // this.items.forEach((e, index) => {
-        //     if(e.text ===  'Autores'){
-        //         let r = `BUSCAR_${e.text.toUpperCase().substring(0,e.text.length -2)}`;
-        //         if(!roles.includes(r)){
-        //             this.items.splice(index,1)
-        //         }
-        //     }else{
-        //         let r = `BUSCAR_${e.text.toUpperCase().substring(0,e.text.length -1)}`;
-        //         if(!roles.includes(r)){
-        //             this.items.splice(index,1)
-        //         }
-        //     }
-            
-            
-        // })
-        this.$refs.tree.$on('node:selected', this.onNodeSelect)
     }
 }
 </script>
 
 <style>
+
+    nav ul {
+        display:flex;
+        flex-direction: column;
+        padding: 0px;
+    }
+
+    nav ul li{
+        cursor: pointer;
+        padding: 8px;
+    }
+
+    nav ul li:hover, .active{
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    nav ul li span{
+        margin-left: 32px;
+        color: #fff;
+        text-decoration: none;
+    }
+
+    nav ul li span:hover{
+        color: #fff;
+        text-decoration: none;
+    }
+    
     .menu {
         grid-area: menu;
         background: linear-gradient(to right, #232526, #414345);
@@ -77,18 +104,8 @@ export default {
         flex-direction: column;
         flex-wrap: wrap;
     }
-    .menu a,
-    .menu a:hover {
-        color: #fff;
-        text-decoration: none;
-    }
-    .menu .tree-node.selected > .tree-content,
-    .menu .tree-node .tree-content:hover {
-        background-color: rgba(255, 255, 255, 0.2);
-    }
-    .tree-arrow.has-child {
-        filter: brightness(2);
-    }
+
+    
     .menu .menu-filter {
         display: flex;
         justify-content: center;
