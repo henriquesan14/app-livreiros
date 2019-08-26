ad60622ded6a98e064af77dab6355b9ba8682b78
Henrique <ricogx@hotmail.com>
2019-08-25 21:27:03 -0300

    refatorado items de menu por role, removido v-if e v-for juntos

--- src/components/template/Menu.vue
+++ src/components/template/Menu.vue
@@ -1,24 +1,21 @@
 <template>
   <aside class="menu" v-show="isMenuVisible">
-    <div class="menu-filter">
-      <i class="fa fa-search fa-lg"></i>
-      <input
-        type="text"
-        placeholder="Digite para filtrar"
-        v-model="filtroMenu"
-        class="filter-field"
-      />
-    </div>
-    <span class="tree-filter-empty" v-if="items.length < 1">Categoria não encontrada</span>
     <nav class="menu-aside">
       <ul>
         <router-link
+        active-class="active"
+        exact
+        to="/dashboard/home"
+        tag="li"
+        >
+        <span><i class="fas fa-home mr-2" ></i>Home</span>
+        </router-link>
+        <router-link
           active-class="active"
           exact
-          v-if="item.visible"
           :to="item.path"
           tag="li"
-          v-for="item in items"
+          v-for="item in activeItems"
           :key="item.id"
         >
           <span>
@@ -39,111 +36,58 @@ export default {
   computed: mapGetters(["isMenuVisible"]),
   data() {
     return {
-      filtroMenu: "",
+      activeItems: [],
       items: [
-        {
-          text: "Home",
-          path: "/dashboard/home",
-          icon: "fa fa-home",
-          visible: true
-        },
         {
           text: "Usuarios",
           path: "/dashboard/usuarios",
           icon: "fa fa-cogs",
-          visible: this.hasRole("BUSCAR_USUARIO")
+          visible: "BUSCAR_USUARIO"
         },
         {
           text: "Livros",
           path: "/dashboard/livros",
           icon: "fa fa-book",
-          visible: this.hasRole("BUSCAR_LIVRO")
+          visible: "BUSCAR_LIVRO"
         },
         {
           text: "Editoras",
           path: "/dashboard/editoras",
           icon: "fas fa-book-open",
-          visible: this.hasRole("BUSCAR_EDITORA")
+          visible: "BUSCAR_EDITORA"
         },
         {
           text: "Autores",
           path: "/dashboard/autores",
           icon: "fas fa-user-cog",
-          visible: this.hasRole("BUSCAR_AUTOR")
+          visible: "BUSCAR_AUTOR"
         },
         {
           text: "Assuntos",
           path: "/dashboard/assuntos",
           icon: "fa fa-comments",
-          visible: this.hasRole("BUSCAR_ASSUNTO")
+          visible: "BUSCAR_ASSUNTO"
         },
         {
           text: "Categorias",
           path: "/dashboard/categorias",
           icon: "fas fa-file-alt",
-          visible: this.hasRole("BUSCAR_DESCRICAO")
+          visible: "BUSCAR_DESCRICAO"
         }
       ]
     };
   },
-  watch: {
-    filtroMenu(newValue, oldValue) {
-      let items = [
-        {
-          text: "Home",
-          path: "/dashboard/home",
-          icon: "fa fa-home",
-          visible: true
-        },
-        {
-          text: "Usuarios",
-          path: "/dashboard/usuarios",
-          icon: "fa fa-cogs",
-          visible: this.hasRole("BUSCAR_USUARIO")
-        },
-        {
-          text: "Livros",
-          path: "/dashboard/livros",
-          icon: "fa fa-book",
-          visible: this.hasRole("BUSCAR_LIVRO")
-        },
-        {
-          text: "Editoras",
-          path: "/dashboard/editoras",
-          icon: "fas fa-book-open",
-          visible: this.hasRole("BUSCAR_EDITORA")
-        },
-        {
-          text: "Autores",
-          path: "/dashboard/autores",
-          icon: "fas fa-user-cog",
-          visible: this.hasRole("BUSCAR_AUTOR")
-        },
-        {
-          text: "Assuntos",
-          path: "/dashboard/assuntos",
-          icon: "fa fa-comments",
-          visible: this.hasRole("BUSCAR_ASSUNTO")
-        },
-        {
-          text: "Categorias",
-          path: "/dashboard/categorias",
-          icon: "fas fa-file-alt",
-          visible: this.hasRole("BUSCAR_DESCRICAO")
-        }
-      ];
-      this.items = items
-        .filter(x => x.visible === true)
-        .filter(x => x.text.toLowerCase().includes(newValue.toLowerCase()));
-    }
+  mounted(){
+    this.initMenu();
   },
   methods: {
-    hasRole(role) {
+    initMenu() {
       let roles = JSON.parse(localStorage.getItem(userKey)).user.permissoes;
-      if (roles.includes(role)) {
-        return true;
-      }
-      return false;
+      this.items.forEach(i => {
+        if(roles.includes(i.visible)){
+          this.activeItems.push(i);
+        }
+      });
     }
   }
 };
@@ -199,35 +143,6 @@ nav ul li span:hover {
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-
-  .menu-filter {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 20px;
-    padding-bottom: 8px;
-    border-bottom: 1px solid #aaa;
-
-    i {
-      color: #aaa;
-      margin-right: 10px;
-      font-size: 0.875rem;
-    }
-  }
-
-  input {
-    color: #ccc;
-    font-size: 0.875rem;
-    border: 0;
-    outline: 0;
-    width: 100%;
-    background: transparent;
-  }
 }
 
-.tree-filter-empty {
-  color: #ccc;
-  font-size: 0.875rem;
-  margin-left: 20px;
-}
 </style>
