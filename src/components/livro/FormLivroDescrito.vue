4b1fb38bcf3b9ad0d6fad3e2005896c4fca3c820
Henrique <ricogx@hotmail.com>
2019-11-08 21:00:21 -0300

    ajustado validação de quantidade de livro descrito

--- src/components/livro/FormLivroDescrito.vue
+++ src/components/livro/FormLivroDescrito.vue
@@ -2,133 +2,135 @@
   <div>
     <Loading :loader="loaderLivro || loaderLivroDesc" />
     <div v-if="!loaderLivro && !loaderLivroDesc">
-      <div class="table-responsive">
-        <table class="table table-hover table-sm table-striped">
-          <thead class="thead-dark">
-            <tr>
-              <th>Título</th>
-              <th>Autor</th>
-              <th>Editora</th>
-              <th>Ano</th>
-              <th>Condicao</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{{livro.tituloLivro}}</td>
-              <td>{{livro.autor.nomeAutor ? livro.autor.nomeAutor : 'N/A'}}</td>
-              <td>{{livro.editora.nomeEditora ? livro.editora.nomeEditora: 'N/A'}}</td>
-              <td>{{livro.anoLivro}}</td>
-              <td>{{livro.condicaoLivro}}</td>
-            </tr>
-          </tbody>
-        </table>
+      <div>
+        <div class="table-responsive">
+          <table class="table table-hover table-sm table-striped">
+            <thead class="thead-dark">
+              <tr>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>Editora</th>
+                <th>Ano</th>
+                <th>Condicao</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{livro.tituloLivro}}</td>
+                <td>{{livro.autor.nomeAutor ? livro.autor.nomeAutor : 'N/A'}}</td>
+                <td>{{livro.editora.nomeEditora ? livro.editora.nomeEditora: 'N/A'}}</td>
+                <td>{{livro.anoLivro}}</td>
+                <td>{{livro.condicaoLivro}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <b-form @submit.prevent="submitLivroDesc()">
+          <b-row>
+            <b-col>
+              <b-form-group label="Código.">
+                <b-form-input size="sm" v-model="livro.idLivro" readonly />
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group
+                label="SubCódigo."
+                :invalid-feedback="invalidFeedBack($v.livroDescrito.subIdLivro)"
+              >
+                <the-mask
+                  mask="SS"
+                  class="form-control form-control-sm"
+                  :readOnly="livroDescrito.idLivroDescrito"
+                  :class="{'is-invalid': submitted && $v.livroDescrito.subIdLivro.$invalid, 'is-valid': submitted && !$v.livroDescrito.subIdLivro.$invalid}"
+                  v-model="livroDescrito.subIdLivro"
+                  maxlength="2"
+                />
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group label="Valor">
+                <b-form-input size="sm" :readonly="true" :value="livro.precoLivro" />
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col md="4">
+              <b-form-group
+                :label="livroDescrito.idLivroDescrito? 'Qtd. Atual' : 'Qtd.'"
+                :invalid-feedback="invalidFeedBack($v.livroDescrito.qtdLivro)"
+              >
+                <the-mask
+                  :readOnly="livroDescrito.idLivroDescrito"
+                  :class="{'is-invalid': submitted && $v.livroDescrito.qtdLivro.$invalid, 'is-valid': submitted && !$v.livroDescrito.qtdLivro.$invalid}"
+                  mask="####"
+                  class="form-control form-control-sm"
+                  v-model="livroDescrito.qtdLivro"
+                />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="livroDescrito.idLivroDescrito">
+              <b-form-group label="Qtd.">
+                <the-mask
+                  mask="####"
+                  class="form-control form-control-sm"
+                  v-model="livroDescrito.qtd"
+                />
+              </b-form-group>
+            </b-col>
+            <b-col v-if="livroDescrito.idLivroDescrito">
+              <b-form-group label="Tipo Movimento">
+                <b-form-select size="sm" v-model="livroDescrito.movimento">
+                  <option v-for="op in options" :key="op.name" :value="op.value">{{op.name}}</option>
+                </b-form-select>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <b-row>
+            <b-col>
+              <b-form-group label="Obs.">
+                <b-form-textarea v-model="livroDescrito.obsLivroDescrito" />
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <b-row v-if="livroDescrito.textLivroDescrito">
+            <b-col>
+              <b-form-group label="Descrição Atual">
+                <b-form-textarea :readonly="true" v-model="livroDescrito.textLivroDescrito" />
+              </b-form-group>
+            </b-col>
+          </b-row>
+          <b-row class="mb-2">
+            <b-col>
+              <b-button type="submit" class="mr-2" variant="success" size="sm">
+                <i class="mr-1 fa fa-save"></i>Salvar
+              </b-button>
+              <router-link tag="b-button" class="btn-dark btn-sm" to="/dashboard/livros">
+                <i class="fa fa-arrow-left mr-1"></i>Voltar
+              </router-link>
+            </b-col>
+          </b-row>
+          <b-badge class="mb-2" variant="danger"><span class="badge-preco">Preço com ajuste: {{precoComAjuste() | currency}}</span></b-badge>
+        </b-form>
       </div>
-
-      <b-form @submit.prevent="submitLivroDesc()">
-        <b-row>
-          <b-col>
-            <b-form-group label="Código.">
-              <b-form-input size="sm" v-model="livro.idLivro" readonly />
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group
-              label="SubCódigo."
-              :invalid-feedback="invalidFeedBack($v.livroDescrito.subIdLivro)"
+      <div>
+        <b-table class="table-sm" :items="descricoes" :fields="fields" striped hover>
+          <template v-slot:cell(actions)="data">
+            <b-form-checkbox-group v-model="livroDescrito.descricoes">
+              <b-form-checkbox :value="data.item.idDescricao"></b-form-checkbox>
+            </b-form-checkbox-group>
+          </template>
+          <template v-slot:cell(categoria_descricao.nomeCategoriaDescricao)="data">
+            <b-badge
+              :variant="badgesCategorias(data.item.categoria_descricao.nomeCategoriaDescricao)"
             >
-              <the-mask
-                mask="SS"
-                class="form-control form-control-sm"
-                :readOnly="livroDescrito.idLivroDescrito"
-                :class="{'is-invalid': submitted && $v.livroDescrito.subIdLivro.$invalid, 'is-valid': submitted && !$v.livroDescrito.subIdLivro.$invalid}"
-                v-model="livroDescrito.subIdLivro"
-                maxlength="2"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group label="Valor">
-              <b-form-input size="sm" :readonly="true" :value="livro.precoLivro" />
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col md="4">
-            <b-form-group
-              :label="livroDescrito.idLivroDescrito? 'Qtd. Atual' : 'Qtd.'"
-              :invalid-feedback="invalidFeedBack($v.livroDescrito.qtdLivro)"
-            >
-              <the-mask
-                :readOnly="livroDescrito.idLivroDescrito"
-                :class="{'is-invalid': submitted && $v.livroDescrito.qtdLivro.$invalid, 'is-valid': submitted && !$v.livroDescrito.qtdLivro.$invalid}"
-                mask="####"
-                class="form-control form-control-sm"
-                v-model="livroDescrito.qtdLivro"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col v-if="livroDescrito.idLivroDescrito">
-            <b-form-group label="Qtd.">
-              <the-mask
-                mask="####"
-                class="form-control form-control-sm"
-                v-model="livroDescrito.qtd"
-              />
-            </b-form-group>
-          </b-col>
-          <b-col v-if="livroDescrito.idLivroDescrito">
-            <b-form-group label="Tipo Movimento">
-              <b-form-select size="sm" v-model="livroDescrito.movimento">
-                <option v-for="op in options" :key="op.name" :value="op.value">{{op.name}}</option>
-              </b-form-select>
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <b-row>
-          <b-col>
-            <b-form-group label="Obs.">
-              <b-form-textarea v-model="livroDescrito.obsLivroDescrito" />
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <b-row v-if="livroDescrito.textLivroDescrito">
-          <b-col>
-            <b-form-group label="Descrição Atual">
-              <b-form-textarea :readonly="true" v-model="livroDescrito.textLivroDescrito" />
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row class="mb-2">
-          <b-col>
-            <b-button type="submit" class="mr-2" variant="success" size="sm">
-              <i class="mr-1 fa fa-save"></i>Salvar
-            </b-button>
-            <router-link tag="b-button" class="btn-dark btn-sm" to="/dashboard/livros">
-              <i class="fa fa-arrow-left mr-1"></i>Voltar
-            </router-link>
-          </b-col>
-        </b-row>
-        <b-badge class="mb-2" variant="danger"><span class="badge-preco">Preço com ajuste: {{precoComAjuste() | currency}}</span></b-badge>
-      </b-form>
-    </div>
-    <div>
-      <b-table class="table-sm" :items="descricoes" :fields="fields" striped hover>
-        <template v-slot:cell(actions)="data">
-          <b-form-checkbox-group v-model="livroDescrito.descricoes">
-            <b-form-checkbox :value="data.item.idDescricao"></b-form-checkbox>
-          </b-form-checkbox-group>
-        </template>
-        <template v-slot:cell(categoria_descricao.nomeCategoriaDescricao)="data">
-          <b-badge
-            :variant="badgesCategorias(data.item.categoria_descricao.nomeCategoriaDescricao)"
-          >
-            <span class="badge-categoria">{{data.item.categoria_descricao.nomeCategoriaDescricao.toUpperCase()}}</span>
-          </b-badge>
-        </template>
-      </b-table>
+              <span class="badge-categoria">{{data.item.categoria_descricao.nomeCategoriaDescricao.toUpperCase()}}</span>
+            </b-badge>
+          </template>
+        </b-table>
+      </div>
     </div>
   </div>
 </template>
@@ -182,7 +184,7 @@ export default {
     return {
       livroDescrito: {
         subIdLivro: { required },
-        qtdLivro: { required, minValue: minValue(1) }
+        qtdLivro: { required, minValue: minValue(0) }
       }
     };
   },
